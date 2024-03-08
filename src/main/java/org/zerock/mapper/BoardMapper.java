97e14fd34acf1b4f193e1fee2021feb213e4994b
juri <ojl98@naver.com>
2024-03-08 16:33:30 +0900

    BoardMapper의 annotation sql문 삭제(주석처리)

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,12 +2,11 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
   //SQL을 어노테이션으로 작성할 때는 반드시 ';'(세미콜론)이 없도록 작성
   //전체 검색
-  @Select("select * from tbl_board where bno > 0")
+  //@Select("select * from tbl_board where bno > 0") - annotation 방식
   public List<BoardVO> getList();
 }
