af38622fce1dbadb95ae591896729536ad7d24da
juri <ojl98@naver.com>
2024-03-08 13:01:52 +0900

    BoardMapper에 SQL 작성

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -1,5 +1,13 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
 public interface BoardMapper {
-  
+  //SQL을 어노테이션으로 작성할 때는 반드시 ';'(세미콜론)이 없도록 작성
+  //전체 검색
+  @Select("select * from tbl_board where bno > 0")
+  public List<BoardVO> getList();
 }
