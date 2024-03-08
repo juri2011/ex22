24b6df82ed24f4173a6526950cfe61c9430330dc
juri <ojl98@naver.com>
2024-03-08 17:04:58 +0900

    BoardMapperTests에 testInsert()단위 테스트 완료

--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -20,4 +21,18 @@ public class BoardMapperTests {
     //람다식
     mapper.getList().forEach(board->log.info(board));
   }
+  
+  @Test
+  public void testInsert() {
+    BoardVO board = new BoardVO();
+    board.setTitle("테스트용 글입니다");
+    board.setContent("내용테스트~~~~!!!");
+    board.setWriter("오주리");
+    
+    mapper.insert(board);
+    
+    //bno의 값이 null로 비어있는 것을 볼 수 있다.
+    log.info(board);
+  }
+  
 }
